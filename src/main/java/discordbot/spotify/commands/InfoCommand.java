2217c362f513e5334b4e1c428c7c0f37dd33b686
Steven Li <steven.li@stonybrook.edu>
2017-10-11 19:29:08 -0400

    Initialized info command.

---
+++ src/main/java/discordbot/spotify/commands/InfoCommand.java
@@ -0,0 +1,40 @@
+package discordbot.spotify.commands;
+
+import de.btobastian.sdcf4j.Command;
+import de.btobastian.sdcf4j.CommandExecutor;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by stebbun on 10/11/2017.
+ */
+public class InfoCommand implements CommandExecutor {
+
+    @Command(aliases = {"!s.info", "!info"}, description = "Displays requested information.",
+            usage = "!s.info [bot|author|time]")
+    public String onInfoCommand(String[] args){
+        //need to specify a tag
+        if (args.length > 1 || args.length == 0) {
+            return "Invalid Arguments.";
+        }
+        else if(args.length == 1){
+            if(args[0].equals("bot")){
+                return "- **Author:** <YourName>\n" +
+                        "- **Language:** Java\n" +
+                        "- **Command-Lib:** sdcf4j";
+            }
+            else if(args[0].equals("author")){
+                return "- **Name:** <YourName>\n" +
+                        "- **Age:** <YourAge>\n" +
+                        "- **Website:** <YourWebsite>";
+            }
+            else if(args[0].equals("time")){
+                SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+                Date currentDate = new Date(System.currentTimeMillis());
+                return "It's" + format.format(currentDate);
+            }
+        }
+        return "Invalid Arguments.";
+    }
+}
---
+++ src/main/java/discordbot/spotify/commands/NewReleasesCommand.java
@@ -0,0 +1,10 @@
+package discordbot.spotify.commands;
+
+import de.btobastian.sdcf4j.CommandExecutor;
+
+/**
+ * Created by stebbun on 10/11/2017.
+ */
+public class NewReleasesCommand implements CommandExecutor {
+
+}
